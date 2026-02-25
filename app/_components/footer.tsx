9101d8ed633b1a69e19004eb26765a4ed08b6edf
Lucio Japeca <luciocecilia20@gmail.com>
2026-02-25 20:36:39 -0300

    feat: add footer componet

---
+++ app/_components/footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return(
+    <footer className="p-7 bg-muted">
+        <p className="text-foreground text-xs font-semibold">
+            &copy; 2026 Aparatus
+        </p>
+        <p className="text-muted-foreground text-xs">
+            Todos os direitos reservados.
+        </p>
+    </footer>
+    )
+}
+
+export default Footer;
